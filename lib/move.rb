0cbd3a5983375174408e14a1c920cde99aa5baae
Anna Moser <anna.s.moser@gmail.com>
2017-03-04 21:41:23 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -7,3 +7,13 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
+def input_to_index(user_input)
+   user_input = user_input.to_i
+   user_input - 1
+ end
+
+ def move(board, index, character="X")
+   if board[index] = " "
+     board[index] = character
+   end
+ end
