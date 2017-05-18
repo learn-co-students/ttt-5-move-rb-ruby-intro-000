bbf4808fdb0f74e9457915cb22cd2ac33478bb64
Colton Poore <coltonpoore12@gmail.com>
2017-05-18 03:08:58 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -6,4 +6,20 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+def input_to_index("1-9")
+  gets.string
+  "1" = board[0]
+  "2" = board[1]
+  "3" = board[2]
+  "4" = board[3]
+  "5" = board[4]
+  "6" = board[5]
+  "7" = board[6]
+  "8" = board[7]
+  "9" = board[8]
+end
+
+
+
+
 # code your input_to_index and move method here!
