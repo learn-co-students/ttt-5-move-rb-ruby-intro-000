f946f0fa025baf1711977f4aaec70102f28bfca9
Tim Freeman <d.timothy.freeman@gmail.com>
2017-07-12 12:28:29 -0400

    all tests passing

--- lib/move.rb
+++ lib/move.rb
@@ -7,3 +7,12 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
+
+def input_to_index(input)
+	int = input.to_i
+	int -= 1
+end
+
+def move(board, index, char="X")
+	board[index] = char
+end
