f37ed82a9a8a2ed8053774227d5858a660bb5038
Daniel Mack <dmmshadow@aol.com>
2017-10-13 19:03:22 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -7,3 +7,10 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
+ def input_to_index(user_input)
+  (1..9).include?(user_input.to_i) ? user_input.to_i - 1 : -1
+  end
+
+  def move(board, pos, gooo = "X")
+   board[pos] = gooo
+  end
