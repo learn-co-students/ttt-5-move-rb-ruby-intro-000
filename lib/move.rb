0872a3608a0a9fef9ae068e9a0ed2e2604c2dcd3
Ted Swedalla <fluffingthevoid@gmail.com>
2017-12-10 20:31:38 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -1,7 +1,3 @@
-puts "Welcome to Tic Tac Toe!"
-
-
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
@@ -11,26 +7,11 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-display_board(board)
-
 def input_to_index(index)
-  puts "Where would you like to go?"
-  input = gets.strip
-  index = input.to_i - 1
+  index.to_i - 1
 end
-
-index = input_to_index(index)
-
 
 def update_array_at_with(array,index,value)
   array[index] = value
 end
-
-update_array_at_with(board,index,"X")
-
-display_board(board)
-
-
-
-
-# code your input_to_index and move method here!
+# code your input_to_index and move method h
