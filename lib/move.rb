988e71f355302e012fee3ed8e4d3c3a586a713c2
Shiping Yi <esp132@gmail.com>
2018-02-19 17:31:20 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -1,9 +1,19 @@
+
+
+# code your input_to_index and move method here!
+def input_to_index(string)
+int=string.to_i-1
+end
+
+def move(board,int,symbol="X")
+
+board[int]=symbol
+end
 def display_board(board)
+
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
-
-# code your input_to_index and move method here!
