58de7d95d14ef7cc9cd1609c76f4cc11238bac59
Nixson Henry <henrynixon.hn@gmail.com>
2019-02-13 23:45:02 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -6,4 +6,17 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+def input_to_index(user_input)
+  index = user_input.to_i-1
+end
+
+def move(board, index, value = "X")
+  board[index] = value
+  return board
+end
 # code your input_to_index and move method here!
+# print "enter an input from 1 to 9"
+# user_input = gets.strip
+# def input_to_index(index)
+#   index = gets.to_i - 1
+# end
