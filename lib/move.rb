cc2d5d744d2e2cecfc56048a8f79764141e11899
Joe Tustin <j.tustin@gmail.com>
2019-03-05 21:56:22 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -1,3 +1,8 @@
+#We'll be writing our #display_board, #input_to_index and #move methods in lib/move.rb
+#and writing the code that interacts with the command line in the bin/move file.
+
+
+
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -7,3 +12,11 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
+
+def input_to_index(input)
+  index = input.to_i - 1
+end
+
+def move(array, index, value = "X")
+  array[index] = value
+end
