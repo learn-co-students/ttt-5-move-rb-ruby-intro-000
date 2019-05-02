c2f558932061509ee667d165dd58c5972080a728
Jason <me@httplife.com>
2019-05-02 07:35:32 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -8,7 +8,7 @@ end
 
 # code your input_to_index and move method here!
 def input_to_index(user_input)
-  converted_input = user_input.to_i - 1
+  user_input.to_i - 1
 end
 
 def move(array, index, value = "X")
