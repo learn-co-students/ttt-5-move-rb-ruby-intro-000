1bac40ae9dc1409e3a771850ef820ec446fc60df
weng lin <wenglin@buffalo.edu>
2019-11-24 03:28:43 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -6,8 +6,8 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def input_to_index(user_input)
-  user_input.to_i - 1
+def input_to_index(input)
+  input.to_i - 1
 end
 
 def move(board, position, character="X")
