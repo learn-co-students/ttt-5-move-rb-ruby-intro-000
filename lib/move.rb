6ee11c226b178347950fe52343ad6fba016776f7
Benjamin Horns <benhorns@gmail.com>
2019-12-10 21:21:41 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -6,4 +6,13 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+def input_to_index(user_input)
+  puts"Where would you like to go?"
+  user_input.to_i - 1
+end
+
+def move(board,position,player = 'X')
+board[position] = player
+end
+
 # code your input_to_index and move method here!
