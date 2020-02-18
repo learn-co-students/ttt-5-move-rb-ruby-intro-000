f0560bdea1733e7e73ef4f222ca0a5126d07fa9c
Tracy Collins <tc@threeceemedia.com>
2020-02-18 00:03:57 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -6,4 +6,15 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-# code your input_to_index and move method here!
+def input_to_index(input)
+  index = input.to_i - 1
+  return index
+end
+
+def move(board, index, cellValue = "X")
+  board[index] = cellValue
+end
+
+def update_array_at_with(array, index, value)
+  array[index] = value
+end
