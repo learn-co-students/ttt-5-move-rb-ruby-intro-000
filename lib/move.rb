3a8c8724f8f273266348e5f390faeea330047a37
Shema Bryan <shema.bryan@jjay.cuny.edu>
2020-04-03 16:18:29 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -6,4 +6,20 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-# code your input_to_index and move method here!
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+def move(board, index, player)
+   board[index] = player
+end
+describe '#move' do
+ it 'does not allow for a default third argument' do
+  board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+  expect {move(board, 2)}.to raise_error(ArgumentError)
+end
+def position_taken?(board, index)
+   if (board[index] == " ") || (board[index] == "") || (board[index] == nil)
+      return false
+   else
+      return true
+   end
