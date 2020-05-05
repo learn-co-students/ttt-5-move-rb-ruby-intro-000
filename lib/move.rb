5d2fb92f2616038a0d39ddb4cc8188a21ef0c6ee
Matthew Blank <msb277@cornell.edu>
2020-05-05 02:43:19 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -13,9 +13,8 @@ end
 # code your input_to_index and move method here!
 
 
-def move(board, num, x_or_o="X")
-  board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-  index = input_to_index(input)
+def move(board, index, x_or_o = "X")
+  board = board
   board[index] = x_or_o
   board
 end
