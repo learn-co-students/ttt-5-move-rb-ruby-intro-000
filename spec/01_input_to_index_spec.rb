d3f4b027af49016cbd4eb177acfa41c6fc450b41
Patrick Cremin <patrickfxcremin@gmail.com>
2020-05-23 20:51:41 +0000

    Done.

--- spec/01_input_to_index_spec.rb
+++ spec/01_input_to_index_spec.rb
@@ -4,23 +4,20 @@ describe '#input_to_index' do
 
   it 'converts a user_input to an integer' do
     user_input = "1"
-    converted_input = input_to_index(user_input)
-    
-    expect(converted_input).to be_a(Integer)
+
+    expect(input_to_index(user_input)).to be_a(Integer)
   end
 
   it 'subtracts 1 from the user_input' do
     user_input = "6"
-    converted_input = input_to_index(user_input)
 
-    expect(converted_input).to be(5)
+    expect(input_to_index(user_input)).to be(5)
   end
 
   it 'returns -1 for strings without integers' do
     user_input = "invalid"
-    converted_input = input_to_index(user_input)
-    
-    expect(converted_input).to be(-1)
+
+    expect(input_to_index(user_input)).to be(-1)
   end
 
 end
