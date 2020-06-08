e1e784fabac59064fd2277ca58173442a100f3c6
Alvee Noor Akand <alvee.akand@gmail.com>
2020-06-08 16:16:18 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -7,3 +7,19 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
+def input_to_index(num_str)
+  valid_str_arr = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+  num_str = num_str.strip
+  
+  if valid_str_arr.include?(num_str)
+    return num_str.to_i - 1 
+  else
+    return -1
+  end
+end
+
+def move(board, player_move_idx, player_char="X")
+  # player_move_idx = input_to_index(player_move_idx)
+  board[player_move_idx] = player_char
+  return board
+end
