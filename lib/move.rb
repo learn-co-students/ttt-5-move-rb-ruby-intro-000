1b86fa7e6d6e88c9f6615e757119e9c92daafa06
Ethan Lee-Tyson <eleetyson@gmail.com>
2020-06-26 00:05:36 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -7,3 +7,21 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
+
+# this method takes the user's input and converts it...
+# to the index they want to access
+def input_to_index(input)
+  input.to_i() - 1
+end
+
+# updates the board according to the user's input
+def move(array, index, character = "X")
+  # update the index for where the user played
+  array[index] = character
+  # return the updated array (the board)
+  puts " #{array[0]} | #{array[1]} | #{array[2]} "
+  puts "-----------"
+  puts " #{array[3]} | #{array[4]} | #{array[5]} "
+  puts "-----------"
+  puts " #{array[6]} | #{array[7]} | #{array[8]} "
+end
