aa4a9d25e0ce349ed99e6b64380f2dae7395e4ef
David Morphew <morphewd@umich.edu>
2020-07-17 17:40:06 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -9,7 +9,7 @@ end
 # code your input_to_index and move method here!
 
 def input_to_index(user_input)
-  converted_input = user_input.to_i - 1
+  user_input.to_i - 1
 end
 
 def move(board, converted_input, char= "X")
