965d32b30df511b3b3a08c9a195edbfc2a6f74b9
Mario Rodrigues <mariorodjr92@gmail.com>
2020-08-11 03:57:34 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -6,4 +6,27 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-# code your input_to_index and move method here!
+def input_to_index(user_input)
+  if user_input == "1"
+    user_input = 0
+  elsif user_input == "2"
+    user_input = 1
+  elsif user_input == "3"
+    user_input = 2
+  elsif user_input == "4"
+    user_input = 3
+  elsif user_input == "5"
+    user_input = 4
+  elsif user_input == "6"
+    user_input = 5
+  elsif user_input == "7"
+    user_input = 6
+  elsif user_input == "8"
+    user_input = 7
+  else
+    return -1
+  end
+end
+
+def move(board, index, char = "X")
+end
