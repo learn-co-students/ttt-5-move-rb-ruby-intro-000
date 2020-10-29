4b2203723bf9fd96c0c01148ed6be71f41426847
Vicki Kieu Nguyen <vickikieunguyen@gmail.com>
2020-10-29 23:04:52 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -1,3 +1,13 @@
+def display_board
+   puts row = ["   " "|" "   " "|" "   "]
+   puts separator = "-----------"
+   puts row
+   puts separator
+   puts row
+end
+
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -5,5 +15,14 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def move(board, index, value = "X")
+   board[index] = value
+end
+
 
 # code your input_to_index and move method here!
