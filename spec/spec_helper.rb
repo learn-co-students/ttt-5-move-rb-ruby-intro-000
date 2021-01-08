302f9030126996314d86b992117d7a852de5c4a0
Jason Sevilla <jesevillaescobar@gmail.com>
2021-01-08 01:15:37 +0000

    Done.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,12 +9,12 @@ end
 def get_variable_from_file(file, variable)
   file_scope = binding
   file_scope.eval(File.read(file))
-  
+
   begin
     return file_scope.local_variable_get(variable)
   rescue NameError
     raise NameError, "local variable `#{variable}' not defined in #{file}."
-  end    
+  end
 end
 
 def capture_puts
