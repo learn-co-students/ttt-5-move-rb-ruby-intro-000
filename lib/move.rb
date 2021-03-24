6c448eb50e91d72393fc3a26556a79885febe803
Kibin <jacobyoo00@hotmail.com>
2021-03-24 21:59:24 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -13,9 +13,11 @@ def input_to_index(user_input)
   user_input.to_i - 1
 end
 
-board = [" ", " ", " "]
+# board = [" ", " ", " "]
 def move(board, index, player = "X")
   board[index] = player
 end
 
-move(board, 0, "X")
+# move(board, 0, "X")
+
+# board
